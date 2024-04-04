50f7bb430b3130f74b4e13d1e8e4017db12ab638
Aniket Pandharabale <aniketicloud@gmail.com>
2024-04-04 20:00:44 +0530

    removed extra stuff

--- src/components/NkButton.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-interface NkButtonProps {
-  label: string;
-}
-
-export const NkButton: FC<NkButtonProps> = ({ label }) => {
-  return <button type="button">{label}</button>;
-};
