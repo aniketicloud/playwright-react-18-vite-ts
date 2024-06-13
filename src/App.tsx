c8850a62600005f83375e939ab052abd56c1bc78
Aniket Pandharabale <aniketicloud@gmail.com>
2024-06-13 18:34:58 +0530

    test: add e2e test for local App.tsx with local server

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
+import { useState } from 'react';
+import './App.css';
+
 const framework = 'Playwright';
 function App() {
-  return <main>{framework} capabilities</main>;
+  const [backgroundColor, setBackgroundColor] = useState('#e74c3c');
+
+  const handleMakeRed = () => {
+    setBackgroundColor('#e74c3c');
+  };
+  const handleMakeGreen = () => {
+    setBackgroundColor('#0ff34c');
+  };
+  const handleMakeYellow = () => {
+    setBackgroundColor('#f1c40f');
+  };
+
+  return (
+    <>
+      <h1 style={{ backgroundColor }}>{framework} capabilities</h1>
+      <span>Current color: {backgroundColor}</span>
+      <div className="btn-group-colors">
+        <button onClick={handleMakeRed}>Red</button>
+        <button onClick={handleMakeGreen}>Green</button>
+        <button onClick={handleMakeYellow}>Yellow</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
